3364377f33c98af02555f91d513b3e6499968c14
othertwink <dbaev9@gmail.com>
2024-09-12 15:15:39 +0300

    Saving filter results to returning array

--- src/main/java/FilterApply.java
+++ src/main/java/FilterApply.java
@@ -4,16 +4,8 @@ interface Filter {
     Object apply(Object o);
 }
 
- class FilterImpl implements Filter {
-
-    @Override
-    public Object apply(Object o) {
-        return o;
-    }
-}
-
 public class FilterApply {
-    public static <T> T[] filter(T[] array, FilterImpl filter) {
+    public static <T> T[] filter(T[] array, Filter filter) {
         T[] filtered = Arrays.copyOf(array, array.length);
         for (int i = 0; i < array.length; i++) {
             filtered[i] = (T) filter.apply(filtered[i]);
